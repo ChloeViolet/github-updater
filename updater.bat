eda97fb4306661933e9ab5a5c6c9cb17c24911db
C K <violet_chloe@proton.me>
2023-05-11 00:46:28 -0400

    script commit test

--- updater.bat
+++ updater.bat
@@ -5,6 +5,7 @@ FOR /F %%x in (updater.bat) DO set /A count += 1
 set /A count -= 8 
 echo ::%count% >> updater.bat 
 git add updater.bat
-git update
-git push origin master
+git commit
+git push
 ::0 
+::1  
